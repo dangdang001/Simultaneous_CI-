69e180312c67e516a8b0d3aae0ff74ba0e1c7bbc
Dangdang001 <donglei.yin@gmail.com>
2018-08-21 15:27:51 -0400

    Delete test_foreach.R

--- test_foreach.R
+++
@@ -1,8 +0,0 @@
-install.packages('doParallel')
-library(doParallel)
-system.time(foreach(i=1:10000) %do% sum(tanh(1:i)))
-
-
-registerDoParallel(cores=10)
-
-system.time(foreach(i=1:10000) %dopar% sum(tanh(1:i)))
